bf17bf36c678a3b0483edf71cc35184c95f0c78b
kannan <chinnakannanr@gmail.com>
2024-01-10 19:58:32 +0530

    ui fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ang-app-face-detector-ai';
+  title = 'face detector ai';
 }
